18c27318afa110c7166603560726a8256d277a16
Carlos Cruz <carlosed.cruz@gmail.com>
2020-03-03 18:22:42 -0500

    MappingJacksonValue - 1st commit

--- src/main/java/com/stacksimplify/restservices/entities/User.java
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -13,15 +13,15 @@ import javax.validation.constraints.Size;
 
 import org.springframework.hateoas.ResourceSupport;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonFilter;
 
 //Entity 
 @Entity
 //@Table(name="User", schema = "usermanagement")
 @Table(name = "user")
-@JsonIgnoreProperties({"firstname","lastname"})
+//@JsonIgnoreProperties({"firstname","lastname"}) -- Static Filtering JsonIgnore
 //public class User { // declaraciòn normal
+@JsonFilter(value = "userFilter")
 public class User extends ResourceSupport { // declaraciòn para HATEOAS
 
 	@Id
@@ -46,7 +46,7 @@ public class User extends ResourceSupport { // declaraciòn para HATEOAS
 	private String role;
 
 	@Column(name = "SSN", length = 50, nullable = false, unique = true)
-	@JsonIgnore
+	// @JsonIgnore -- Static Filtering JsonIgnore
 	private String ssn;
 
 	@OneToMany(mappedBy = "user")
