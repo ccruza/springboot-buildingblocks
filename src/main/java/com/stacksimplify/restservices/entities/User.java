a33b83473fef08a5f106f8d1874955d67960a3fa
Carlos Cruz <carlosed.cruz@gmail.com>
2020-03-03 22:46:30 -0500

    Versioning - 1st commit

--- src/main/java/com/stacksimplify/restservices/entities/User.java
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -61,6 +61,9 @@ public class User extends ResourceSupport { // declaraciòn para HATEOAS
 	@JsonView(Views.Internal.class)
 	private List<Order> orders;
 
+	@Column(name = "ADDRESS")
+	private String address;
+
 	// No Argument Constructor
 	public User() {
 	}
@@ -69,7 +72,7 @@ public class User extends ResourceSupport { // declaraciòn para HATEOAS
 	public User(Long userid,
 			@NotEmpty(message = "Username is Mandatory field. Please provide username") String username,
 			@Size(min = 2, message = "FirstName should have atleast 2 characters") String firstname, String lastname,
-			String email, String role, String ssn, List<Order> orders) {
+			String email, String role, String ssn, List<Order> orders, String address) {
 		super();
 		this.userid = userid;
 		this.username = username;
@@ -79,6 +82,7 @@ public class User extends ResourceSupport { // declaraciòn para HATEOAS
 		this.role = role;
 		this.ssn = ssn;
 		this.orders = orders;
+		this.address = address;
 	}
 
 	// Getters and Setters
@@ -146,11 +150,20 @@ public class User extends ResourceSupport { // declaraciòn para HATEOAS
 		this.orders = orders;
 	}
 
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 	// To String
 	@Override
 	public String toString() {
 		return "User [userid=" + userid + ", username=" + username + ", firstname=" + firstname + ", lastname="
-				+ lastname + ", email=" + email + ", role=" + role + ", ssn=" + ssn + ", orders=" + orders + "]";
+				+ lastname + ", email=" + email + ", role=" + role + ", ssn=" + ssn + ", orders=" + orders
+				+ ", address=" + address + "]";
 	}
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into user values(101,'carlos.cruz@pe.ey.com','Carlos','Cruz','admin','SSN-101','ccruz');
-insert into user values(102,'juan.perez@pe.ey.com','Juan','Perez','admin','SSN-102','jperez');
-insert into user values(103,'maria.db@pe.ey.com','Maria','DB','admin','SSN-103','mariadb');
+insert into user values(101,'Miami','carlos.cruz@pe.ey.com','Carlos','Cruz','admin','SSN-101','ccruz');
+insert into user values(102,'New York','juan.perez@pe.ey.com','Juan','Perez','admin','SSN-102','jperez');
+insert into user values(103,'California','maria.db@pe.ey.com','Maria','DB','admin','SSN-103','mariadb');
 insert into orders values( 2001, 'order11', 101);        
 insert into orders values( 2002, 'order12', 101);
 insert into orders values( 2003, 'order13', 101);
